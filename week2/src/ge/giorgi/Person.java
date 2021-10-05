058facc69089d22127c7fd3947083e709a209b3d
giorgikeburia <giorgikeburia1@gmail.com>
2021-10-05 22:06:20 +0400

    week 2

--- week2/src/ge/giorgi/Person.java
+++ week2/src/ge/giorgi/Person.java
@@ -15,9 +15,31 @@ public class Person {
     void setAgeFromString(String age){
         this.age = Integer.parseInt(age);
     }
+    String sayMyName(int n){
+        StringBuilder st = new StringBuilder();
+        for (int i = 0; i < n; i++){
+            st.append(name);
+            if(i < n-1){
+                st.append(',').append(' ');
+            }
+        }
+        return st.toString();
+    }
+    String piramideBuild(int height, String symbol){
+        StringBuilder myStr = new StringBuilder();
+        for (int i = 0; i < height; i++){
+            myStr.append(" ".repeat(height - 1));
+            ;
+            myStr.append(String.valueOf(symbol).repeat(i + 1));
+            ;
+            myStr.append('\n');
+        }
+        return myStr.toString();
+
+    }
 
     @Override
     public String toString() {
-        return "Person: name " + name + ", age = " + age;
+        return "Person: name " + name + ", age : " + age + ".";
     }
 }
--- week2/src/ge/giorgi/launcher.java
+++ week2/src/ge/giorgi/launcher.java
@@ -1,12 +1,17 @@
 package ge.giorgi;
 
 public class launcher {
-    public static void main (String[] args){
-        // --------------------------------------  1  ---------------------------------
-        Person john_smith = new Person("John Smith",21);
-        john_smith.setAgeFromString("3443");
-        System.out.println(john_smith);
+    public static void main (String[] args) {
+        // --------------------------------------  setAgeFromString  ---------------------------------
+        Person baby_yoda = new Person("Baby Yoda", 21);
+        baby_yoda.setAgeFromString("3443");
 
-        // --------------------------------------  2  ---------------------------------
+        // --------------------------------------  To String  ---------------------------------
+        System.out.println(baby_yoda);
+
+        // --------------------------------------  sayMyName  ---------------------------------
+        System.out.println(baby_yoda.sayMyName(5));
+
+        System.out.println(baby_yoda.piramideBuild(4,"\uD83C\uDDEC\uD83C\uDDEA"));
     }
 }
